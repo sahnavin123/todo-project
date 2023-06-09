0950cda9e05d8c751594f23565d63fa9562fa782
navin <95286412+sahnavin123@users.noreply.github.com>
2023-06-09 16:53:00 +0530

    added error msg and used camel case naming

--- src/createToDo.js
+++ src/createToDo.js
@@ -2,34 +2,103 @@ import { parseISO, startOfToday } from "date-fns";
 import { clearForm } from "./domManipulation.js";
 import { saveToDoToLocal } from "./localStorage.js";
 
-export const createToDo = () => {
-  let title = document.getElementById("Title").value;
-  let description = document.getElementById("Description").value;
-  let dueDate = document.getElementById("DueDate").value;
-  let priority = document.getElementById("Priority").value;
-  let notes = document.getElementById("Notes").value;
+const showError = (elementId, errorMessage) => {
+  const errorElement = document.getElementById(elementId);
+  errorElement.textContent = errorMessage;
+};
 
-  if (title === "" || description === "" || dueDate === "") {
-    alert(
-      "Title, Description, and Due Date are required fields, please enter values!"
+const clearError = (elementId) => {
+  const errorElement = document.getElementById(elementId);
+  errorElement.textContent = "";
+};
+
+const addFieldListeners = () => {
+  const titleField = document.getElementById("title");
+  const descriptionField = document.getElementById("description");
+  const dueDateField = document.getElementById("dueDate");
+
+  titleField.addEventListener("input", () => {
+    clearError("title-error");
+  });
+
+  descriptionField.addEventListener("input", () => {
+    clearError("description-error");
+  });
+
+  dueDateField.addEventListener("input", () => {
+    clearError("dueDate-error");
+  });
+};
+
+export const createToDo = () => {
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  const dueDate = document.getElementById("dueDate").value;
+  const priority = document.getElementById("priority").value;
+  const notes = document.getElementById("notes").value;
+
+  const errorFields = [];
+
+  clearError("title-error");
+  clearError("description-error");
+  clearError("dueDate-error");
+
+  if (title === "") {
+    showError(
+      "title-error",
+      "Title is a required field, please enter a value!"
     );
+    errorFields.push("title");
+  }
+
+  if (description === "") {
+    showError(
+      "description-error",
+      "Description is a required field, please enter a value!"
+    );
+    errorFields.push("description");
+  }
+
+  if (dueDate === "") {
+    showError(
+      "dueDate-error",
+      "Due date is a required field, please enter a value!"
+    );
+    errorFields.push("dueDate");
+  }
+
+  if (errorFields.length > 0) {
     return;
   }
 
   if (title.length > 30 && description.length > 50) {
-    alert("please enter title less than 30 character and description less than 50 character");
+    showError(
+      "title-error",
+      "Please enter a title with less than 30 characters."
+    );
+    showError(
+      "description-error",
+      "Please enter a description with less than 50 characters."
+    );
     return;
   } else if (title.length > 30) {
-    alert("please enter title less than 30 character");
+    showError(
+      "title-error",
+      "Please enter a title with less than 30 characters."
+    );
     return;
   } else if (description.length > 50) {
-    alert("please enter description less than 50 character");
+    showError(
+      "description-error",
+      "Please enter a description with less than 50 characters."
+    );
     return;
   }
 
   if (parseISO(dueDate) < startOfToday()) {
-    alert(
-      "You have entered a date that has already passed!  Please enter a date greater than or equal to today."
+    showError(
+      "dueDate-error",
+      "You have entered a date that has already passed! Please enter a date greater than or equal to today."
     );
     return;
   }
@@ -40,3 +109,5 @@ export const createToDo = () => {
 
   return todo;
 };
+
+addFieldListeners();
--- src/localStorage.js
+++ src/localStorage.js
@@ -1,9 +1,9 @@
 import { displayToDo } from "./domManipulation.js";
 
-export function saveToDoToLocal(todo) {
+export const saveToDoToLocal = (todo) => {
   const todos = JSON.parse(localStorage.getItem("todos")) || [];
   todos.push(todo);
   localStorage.setItem("todos", JSON.stringify(todos));
   displayToDo();
   return todo;
-}
+};
